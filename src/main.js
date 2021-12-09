7549422114f6ceec98c436adb0bf659861608b38
kangakiles88 <kang.akiles88@gmail.com>
2021-12-09 20:26:14 +0900

    Create project

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import loadImage from './plugins/loadImage'
+import router from './routes'
+import store from './store'
+
+createApp(App)
+.use(router)
+.use(store)
+.use(loadImage)
+.mount('#app')

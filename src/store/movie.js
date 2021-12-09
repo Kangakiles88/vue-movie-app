cd8b9a53fbed7bb62081f5f693245d1c720c8399
kangakiles88 <kang.akiles88@gmail.com>
2021-12-09 22:36:37 +0900

    Modify server port number

--- src/store/movie.js
+++ src/store/movie.js
@@ -23,7 +23,6 @@ export default {
             Object.keys(payload).forEach(key => {
                 state[key] = payload[key]
             })
-
         },
         resetMovies(state) {
             state.movies = []
@@ -79,7 +78,7 @@ export default {
                         })
                     }
                 }
-            } catch (message) {
+            } catch ({ message }) {
                 commit('updateState', {
                     movies: [],
                     message
@@ -117,23 +116,6 @@ export default {
     }
 }
 
-function _fetchMovie (payload) {
-    const { title, type, year, page, id} = payload
-    const OMDB_API_KEY = '87b145c0' 
-    const url = id 
-        ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
-        : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-
-    return new Promise((resolve, reject) => {
-        axios.get(url)
-            .then(res => {
-                if(res.data.Error) {
-                    reject(res.data.Error)
-                }
-                resolve(res)
-            })
-            .catch(err => {
-                reject(err.message)
-            })
-    })
+async function _fetchMovie (payload) {
+    return await axios.post("/.netlify/functions/movie", payload)
 }
